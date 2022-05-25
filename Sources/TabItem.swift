690f63bba507d85b807e126f59ceca792c8c69ec
Radu Ursache <radu@ursache.ro>
2022-05-25 17:47:02 +0300

    - added support for any image in Tabs - added `iconSelected` to customize the selected Tab icon

--- Sources/TabItem.swift
+++ Sources/TabItem.swift
@@ -5,15 +5,18 @@
 //
 
 import Foundation
+import UIKit
 
 public struct TabItem: Codable, Equatable, Hashable {
 	public let index: Int
 	public let title: String
 	public let icon: String
+	public let iconSelected: String?
 	
-	public init(index: Int, title: String, icon: String) {
+	public init(index: Int, title: String, icon: String, iconSelected: String? = nil) {
 		self.index = index
 		self.title = title
 		self.icon = icon
+		self.iconSelected = iconSelected
 	}
 }
--- Sources/TabItemView.swift
+++ Sources/TabItemView.swift
@@ -16,6 +16,28 @@ public struct TabItemView: View {
 		return selectedTab == model
 	}
 	
+	private var tabTitle: String {
+		return String(model.title.prefix(15))
+	}
+	
+	private var tabIcon: UIImage {
+		var imageName = model.icon
+		
+		if isSelected {
+			imageName = model.iconSelected ?? model.icon
+		}
+		
+		return (UIImage(systemName: imageName) ?? UIImage(named: imageName)!).withRenderingMode(.alwaysTemplate)
+	}
+	
+	private var tintColor: Color {
+		if isSelected {
+			return selectedColor ?? .accentColor
+		}
+		
+		return .gray
+	}
+	
 	public var body: some View {
 		Button {
 			selectedTab = model
@@ -23,22 +45,48 @@ public struct TabItemView: View {
 			VStack(alignment: .center, spacing: 0) {
 				Spacer()
 				
-				Image(systemName: model.icon)
+				Image(uiImage: tabIcon)
+					.if(!tabIconIsSfSymbol(), transform: {
+						$0.resizable()
+					})
 					.aspectRatio(contentMode: .fit)
 					.font(.system(size: 22))
-					.foregroundColor(isSelected ? (selectedColor ?? .accentColor) : .gray)
+					.foregroundColor(tintColor)
+					.if(!tabIconIsSfSymbol(), transform: {
+						$0.padding(4)
+					})
 					.frame(width: 32, height: 32, alignment: .center)
 
-				Text(String(model.title.prefix(15)))
-					.font(.system(size: 14))
+				Text(tabTitle)
+					.font(.system(size: 13))
 					.multilineTextAlignment(.center)
 					.fixedSize()
-					.foregroundColor(isSelected ? (selectedColor ?? .accentColor) : .gray)
+					.foregroundColor(tintColor)
 
 				Spacer()
 			}
-			.frame(height: 58)
+			.frame(height: 56)
 		}
 		.frame(width: 45)
+	}
+}
+
+extension TabItemView {
+	func tabIconIsSfSymbol() -> Bool {
+		if isSelected {
+			return UIImage(systemName: model.iconSelected ?? model.icon) != nil
+		}
+		return UIImage(systemName: model.icon) != nil
+	}
+}
+
+fileprivate extension View {
+	@ViewBuilder func `if`<Transform: View>(_ condition: Bool, transform: (Self) -> Transform) -> some View {
+		if condition {
+			transform(self)
+		}
+		else {
+			self
+		}
 	}
 }
